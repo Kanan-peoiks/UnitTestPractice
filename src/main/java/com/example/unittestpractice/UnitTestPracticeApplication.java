c9e880b81207eb2683662edbd44fc9c00d5ee248
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-03-02 23:00:39 +0400

    first commit

---
+++ src/main/java/com/example/unittestpractice/UnitTestPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.unittestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitTestPracticeApplication.class, args);
+    }
+
+}
