2485fd97dff802c0dd426fdfa50829238bee4617
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-03-03 20:54:59 +0400

    feat: add InventoryService and InventoryServiceImpl

---
+++ src/main/java/com/example/unittestpractice/service/InventoryService.java
@@ -0,0 +1,5 @@
+package com.example.unittestpractice.service;
+
+public interface InventoryService {
+    void checkInStock(String bookId);
+}
---
+++ src/main/java/com/example/unittestpractice/service/serviceImpl/InventoryServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.unittestpractice.service.serviceImpl;
+
+import com.example.unittestpractice.service.InventoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryServiceImpl implements InventoryService {
+    @Override
+    public void checkInStock(String bookId) {
+    }
+}
