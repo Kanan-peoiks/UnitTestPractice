402de546b1829de247cddc514f5e363df3066f4b
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-03-03 20:53:44 +0400

    feat: add Order and OrderStatus

---
+++ src/main/java/com/example/unittestpractice/entity/Order.java
@@ -0,0 +1,66 @@
+package com.example.unittestpractice.entity;
+
+import jakarta.persistence.*;
+import jdk.jfr.DataAmount;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String bookId;
+
+    @Column(nullable = false)
+    private String customerId;
+
+    @Column(nullable = false)
+    private double amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
---
+++ src/main/java/com/example/unittestpractice/entity/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.example.unittestpractice.entity;
+
+public enum OrderStatus {
+    PENDING,
+    COMPLETED,
+    FAILED
+}
